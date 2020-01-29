4d912a9677e47b68966bc988451fca631e61da50
Annie Ren <annieren35@gmail.com>
2020-01-29 01:31:25 -0500

    Updates

---
+++ precache-manifest.ecedbfb131d2e159fd51b2fa16ec8d50.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eba24222ce6e661e11d011e91212671",
+    "url": "/personal-website-v2/index.html"
+  },
+  {
+    "revision": "c703ba4e3019e874d2af",
+    "url": "/personal-website-v2/static/css/main.08877ebc.chunk.css"
+  },
+  {
+    "revision": "5fa727ef2f2b2c1deb39",
+    "url": "/personal-website-v2/static/js/2.5d2c1169.chunk.js"
+  },
+  {
+    "revision": "c703ba4e3019e874d2af",
+    "url": "/personal-website-v2/static/js/main.af107d17.chunk.js"
+  },
+  {
+    "revision": "4562bf69b9833d334e72",
+    "url": "/personal-website-v2/static/js/runtime~main.8dd6db9f.js"
+  },
+  {
+    "revision": "b0b8d4c31c74d9378bf6cbbb098b5347",
+    "url": "/personal-website-v2/static/media/beach.b0b8d4c3.jpg"
+  },
+  {
+    "revision": "f358ca629d3cb29a394ef9bdbab25bf3",
+    "url": "/personal-website-v2/static/media/childhood.f358ca62.jpg"
+  },
+  {
+    "revision": "87de045f7b4670c1c26ae420ac975e20",
+    "url": "/personal-website-v2/static/media/cooking.87de045f.jpg"
+  },
+  {
+    "revision": "9bc9a1e57c08677747b25412048ac246",
+    "url": "/personal-website-v2/static/media/cube.9bc9a1e5.png"
+  },
+  {
+    "revision": "30d38bbd170d7c3f18f173e4a4863197",
+    "url": "/personal-website-v2/static/media/epilogue.30d38bbd.jpg"
+  },
+  {
+    "revision": "1e13848abc3c9b9359b4e34364adde6f",
+    "url": "/personal-website-v2/static/media/firework.1e13848a.jpg"
+  },
+  {
+    "revision": "7ef3c2330bfcb9d04d0f6a32b3fc8f45",
+    "url": "/personal-website-v2/static/media/fun.7ef3c233.jpg"
+  },
+  {
+    "revision": "e9a2bffd0bad3185da809d597ad3caed",
+    "url": "/personal-website-v2/static/media/highschool.e9a2bffd.jpg"
+  },
+  {
+    "revision": "2c76781e0f009968e3cf3994e0826a30",
+    "url": "/personal-website-v2/static/media/palace.2c76781e.jpg"
+  },
+  {
+    "revision": "e430379c71d4ce92fad027766df447a5",
+    "url": "/personal-website-v2/static/media/perspective.e430379c.png"
+  },
+  {
+    "revision": "118ab7b731d6849452d544959332ae4b",
+    "url": "/personal-website-v2/static/media/physics.118ab7b7.png"
+  },
+  {
+    "revision": "d9ed2348ee7cfa0721eab4255223ba5b",
+    "url": "/personal-website-v2/static/media/plain.d9ed2348.jpg"
+  },
+  {
+    "revision": "b4f432a92788b8b7f4b28e83ff570892",
+    "url": "/personal-website-v2/static/media/pnp.b4f432a9.png"
+  },
+  {
+    "revision": "d6843159b3a7fdf3cb57f3f1aa00e3f8",
+    "url": "/personal-website-v2/static/media/present.d6843159.jpg"
+  },
+  {
+    "revision": "e0ca1cc9ec9ee052e9d12c6a24982a03",
+    "url": "/personal-website-v2/static/media/tok.e0ca1cc9.png"
+  },
+  {
+    "revision": "52a5e0062820eeb3caf3bfdd258e43dc",
+    "url": "/personal-website-v2/static/media/vancouver.52a5e006.jpg"
+  }
+]);
